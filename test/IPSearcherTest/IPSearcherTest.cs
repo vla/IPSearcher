e1f70076b4bf8309ce3bfa671900794206142d2e
v.la <v.la@live.cn>
2018-07-11 22:49:18 +0800

    测试生成IP库的完整性

--- test/IPSearcherTest/IPSearcherTest.cs
+++ test/IPSearcherTest/IPSearcherTest.cs
@@ -24,9 +24,49 @@ namespace IPSearcherTest
             var searcher = IpSearcherFactory.GetSearcher(dataSource);
 
             var result = searcher.Search(IpLocationHelper.IPv4ToInteger("14.123.238.167"));
-           
+
             Assert.NotNull(result);
             Assert.Equal("广东省", result.Province);
+
+            dataSource.Dispose();
+        }
+
+        [Fact]
+        public void Check_ALL_Match()
+        {
+            var filename = Path.Combine(AppContext.BaseDirectory, "ip.txt");
+            var dest = Path.Combine(AppContext.BaseDirectory, "ip2.dat");
+            var data = Helper.GetIPList(filename).ToArray();
+
+            using (var fs = File.Create(dest))
+            {
+                IpSearcherFactory.Generate(Helper.GetIPList(filename).ToArray(), fs);
+            }
+
+            var dataSource = new MemoryDataSource(dest);
+
+            var searcher = IpSearcherFactory.GetSearcher(dataSource);
+
+            IpLocation result;
+
+            foreach (var info in data)
+            {
+                result = searcher.Search(info.MinIP);
+
+                Assert.Equal(info.Country, result.Country ?? string.Empty);
+                Assert.Equal(info.Province, result.Province ?? string.Empty);
+                Assert.Equal(info.City, result.City ?? string.Empty);
+
+                if (info.Isp == "内网IP")
+                {
+                    Assert.Equal("保留地址", result.Isp ?? string.Empty);
+                }
+                else
+                {
+                    Assert.Equal(info.Isp, result.Isp ?? string.Empty);
+                }
+
+            }
 
             dataSource.Dispose();
         }
