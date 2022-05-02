2dc5c5a8e56251a5f8898f48b8430059e26999e4
v.la <v.la@live.cn>
2022-05-02 22:22:57 +0800

    refactor: bump to 1.0.5
    * 更新IP数据库
    * 目标框架从net462起步

--- build.cmd
+++ build.cmd
@@ -4,11 +4,11 @@ if exist %artifacts%  rd /q /s %artifacts%
 
 call dotnet restore src/IPSearcher
 
-call dotnet build src/IPSearcher -f net40 -c Release -o %artifacts%\net40
+call dotnet build src/IPSearcher -f net462 -c Release -o %artifacts%\net462
 call dotnet build src/IPSearcher -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 
 
-call dotnet build src/IPSearcher.Data -f net40 -c Release -o %artifacts%\net40
+call dotnet build src/IPSearcher.Data -f net462 -c Release -o %artifacts%\net462
 call dotnet build src/IPSearcher.Data -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 
 call dotnet pack src/IPSearcher -c release -o %artifacts%
--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ mkdir -p ${artifacts}
 dotnet restore src/IPSearcher
 dotnet restore src/IPSearcher.Data
 
-dotnet build src/IPSearcher -f net40 -c Release -o ${artifacts}/net40
+dotnet build src/IPSearcher -f net462 -c Release -o ${artifacts}/net462
 dotnet build src/IPSearcher -f netstandard2.0 -c Release -o ${artifacts}/netstandard2.0
 
-dotnet build src/IPSearcher.Data -f net40 -c Release -o ${artifacts}/net40
+dotnet build src/IPSearcher.Data -f net462 -c Release -o ${artifacts}/net462
 dotnet build src/IPSearcher.Data -f netstandard2.0 -c Release -o ${artifacts}/netstandard2.0
 
